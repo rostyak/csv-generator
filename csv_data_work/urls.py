16c7bf843339e3348f5a55752272e831f546186c
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-12-10 20:10:31 +0200

    Create base index.html page, make it work

--- csv_data_work/urls.py
+++ csv_data_work/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
+from csv_data_work.views import index
 
 urlpatterns = [
-    # path("", index, name="index"),
+    path("", index, name="index"),
 ]
 
 app_name = "csv_data_work"
--- csv_data_work/views.py
+++ csv_data_work/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "csv_data_work/index.html")
