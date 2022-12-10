ec7a7857468fdda14917ec314b5921b42fb8bb4d
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-12-10 20:09:12 +0200

    Add templates directory

--- csv_generator/settings.py
+++ csv_generator/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "csv_generator.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
